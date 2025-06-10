15048650bbb533821eca17f8634f3b6628b9fa0b
Otávio Seidinger <taloswi@gmail.com>
2025-06-10 11:23:29 -0300

    arrumando alguma coisa ai

--- pages/areas/script.js
+++ pages/areas/script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', async function () {
         const grid = document.querySelector(".rides-grid");
         grid.innerHTML = grid.innerHTML + `
             <div class="ride-card">
-                <div class="ride-image" style="background-image: url('imagem/bugabalum.jpg')"></div>
+                <div class="ride-image" style="background-image: url('../../imagem/${brinquedo.image}')"></div>
                 <div class="ride-info">
                     <h3 class="ride-name">${brinquedo.name}</h3>
                     <div class="ride-time">${brinquedo.waiting_time} min</div>
